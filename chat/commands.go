9840a71efbc21af2a57c7ea3dfa921981a55e478
desalutare <desalutare@gmail.com>
2026-01-03 11:04:50 +0000

    moved helper functions to the commnds file

---
+++ chat/commands.go
@@ -0,0 +1,99 @@
+package chat
+
+import (
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+func (c *ChatScreenModel) updateInputs(msg tea.Msg) tea.Cmd {
+	var cmd tea.Cmd
+
+	if c.State.IsSearchMode {
+		c.Inputs.SearchUserInput, cmd = c.Inputs.SearchUserInput.Update(msg)
+	} else {
+		c.Inputs.ChatAreaInput, cmd = c.Inputs.ChatAreaInput.Update(msg)
+	}
+
+	return cmd
+}
+
+func (c *ChatScreenModel) handleKeyMsg(msg tea.KeyMsg) (tea.Model, tea.Cmd) {
+	switch msg.String() {
+	case "ctrl+c":
+		return c, tea.Quit
+	case "esc":
+		if c.State.IsSearchMode {
+			c.State.IsSearchMode = false
+			c.State.SearchMessage = ""
+			return c, c.Inputs.ChatAreaInput.Focus()
+		}
+	case "ctrl+s":
+		c.State.IsSearchMode = true
+		return c, c.Inputs.SearchUserInput.Focus()
+	case "enter":
+		return c.handleEnter()
+	}
+	return c, nil
+}
+
+func (c *ChatScreenModel) limitMessages() {
+	// TODO: изменить способ хранения сообщений
+	const MaxMessages = 200
+	if len(c.Messages) > MaxMessages {
+		c.Messages = c.Messages[len(c.Messages)-MaxMessages:]
+	}
+}
+
+func (c *ChatScreenModel) handleEnter() (tea.Model, tea.Cmd) {
+	if c.State.IsSearchMode {
+		return c.handleSearch()
+	}
+	return c.handleSendMessage()
+}
+
+func (c *ChatScreenModel) handleSearch() (tea.Model, tea.Cmd) {
+	username := c.Inputs.SearchUserInput.Value()
+	if username == "" {
+		return c, nil
+	}
+
+	c.State.SearchMessage = "Searching..."
+	return c, searchUserCmd(c.Token, username)
+}
+
+func (c *ChatScreenModel) handleSendMessage() (tea.Model, tea.Cmd) {
+	text := c.Inputs.ChatAreaInput.Value()
+	if text == "" {
+		return c, nil
+	}
+
+	msg := Message{
+		SenderID:   c.UserID,
+		ReceiverID: c.State.ReceiverID,
+		Text:       text,
+	}
+
+	c.Messages = append(c.Messages, msg)
+	c.limitMessages()
+
+	if err := c.WsClient.SendMessage(msg.ReceiverID, msg.Text); err != nil {
+		c.State.Error = err
+	}
+
+	c.Inputs.ChatAreaInput.SetValue("")
+	return c, nil
+}
+
+func (c *ChatScreenModel) handleSearchResult(msg searchResultMsg) (tea.Model, tea.Cmd) {
+	if msg.Err != nil {
+		c.State.SearchMessage = "Error: " + msg.Err.Error()
+		return c, nil
+	}
+
+	c.State.ReceiverID = msg.UserID
+	c.Messages = []Message{}
+	c.State.SearchMessage = fmt.Sprintf("Found user: %s (ID: %d)", msg.Username, msg.UserID)
+	c.State.IsSearchMode = false
+	return c, c.Inputs.ChatAreaInput.Focus()
+}
